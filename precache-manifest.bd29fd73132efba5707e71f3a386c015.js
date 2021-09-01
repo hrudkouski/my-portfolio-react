4503bd3c9f2cf5828714148e1c2d5a579816e2d3
hrudkouski <aprilshower19@gmail.com>
2021-09-01 19:09:55 +0300

    Updates

--- precache-manifest.bd29fd73132efba5707e71f3a386c015.js
+++ precache-manifest.bd29fd73132efba5707e71f3a386c015.js
@@ -1,43 +1,47 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fb2a6e4dce808ed4fa7790ce9214e40c",
+    "revision": "96322a4faeedc8e720632e61f18cf35c",
     "url": "/my-portfolio-react/index.html"
   },
   {
-    "revision": "cb02109cd90e3e39086d",
-    "url": "/my-portfolio-react/static/css/main.772030c1.chunk.css"
+    "revision": "e90c33bea2aed8c9e9cd",
+    "url": "/my-portfolio-react/static/css/main.eb8f49ba.chunk.css"
   },
   {
-    "revision": "7e756bee845c18c352cf",
-    "url": "/my-portfolio-react/static/js/2.f57519c3.chunk.js"
+    "revision": "ce5023addbf10f8695ec",
+    "url": "/my-portfolio-react/static/js/2.f73f92d8.chunk.js"
   },
   {
-    "revision": "cb02109cd90e3e39086d",
-    "url": "/my-portfolio-react/static/js/main.dd547d7c.chunk.js"
+    "revision": "e90c33bea2aed8c9e9cd",
+    "url": "/my-portfolio-react/static/js/main.6f8a7b89.chunk.js"
   },
   {
     "revision": "d00a48d3648baed5d6a2",
     "url": "/my-portfolio-react/static/js/runtime-main.4cccd3f2.js"
   },
   {
-    "revision": "c128f552a49238a70f28d88c384aa96c",
-    "url": "/my-portfolio-react/static/media/ava.c128f552.webp"
+    "revision": "cc44e7fc096c86cd4e4ab22d51db59e3",
+    "url": "/my-portfolio-react/static/media/Front End Developer CV Resume - Mikita Grudkowski.cc44e7fc.pdf"
+  },
+  {
+    "revision": "a7f85966c42a98fed2cc33a70429170f",
+    "url": "/my-portfolio-react/static/media/ava.a7f85966.webp"
   },
   {
     "revision": "ebd61dd53daae83cfb413418138bdb5a",
     "url": "/my-portfolio-react/static/media/codewars.ebd61dd5.png"
   },
   {
-    "revision": "3a9f9f830541660d0f5347b37d7b2642",
-    "url": "/my-portfolio-react/static/media/counter.3a9f9f83.webp"
+    "revision": "b4f8f0d39a5f464d52ea6e94abf7db83",
+    "url": "/my-portfolio-react/static/media/counter.b4f8f0d3.webp"
   },
   {
     "revision": "03a386d452e0416f43440a762c0096d9",
     "url": "/my-portfolio-react/static/media/cssLogo.03a386d4.svg"
   },
   {
-    "revision": "915f4608f45486803e6265b28d291024",
-    "url": "/my-portfolio-react/static/media/dogGame.915f4608.png"
+    "revision": "96d8a9826e7e7f5cf92a0ea48c8babe5",
+    "url": "/my-portfolio-react/static/media/fridayTeam.96d8a982.webp"
   },
   {
     "revision": "16467be478499a05bad362cb430758a2",
@@ -56,10 +60,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/my-portfolio-react/static/media/jsLogo.87711407.svg"
   },
   {
-    "revision": "b9a0978a85b17c5d63cdc4a0fe581d39",
-    "url": "/my-portfolio-react/static/media/portfolio.b9a0978a.webp"
-  },
-  {
     "revision": "ee61ca49ec2188e4ac026017e8b315b7",
     "url": "/my-portfolio-react/static/media/reactLogo.ee61ca49.svg"
   },
@@ -68,12 +68,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/my-portfolio-react/static/media/reduxLogo.85772647.svg"
   },
   {
+    "revision": "73e48c1a212be5037702bc148126a567",
+    "url": "/my-portfolio-react/static/media/socialNetwork.73e48c1a.webp"
+  },
+  {
     "revision": "11a0e4282e76c36c22b3a780dfa3f68e",
     "url": "/my-portfolio-react/static/media/telegram.11a0e428.svg"
   },
   {
-    "revision": "5d8b3b10190d62e59c48b8899194e0ce",
-    "url": "/my-portfolio-react/static/media/toDoList.5d8b3b10.webp"
+    "revision": "694f5817583c07435a50505f8ae319ca",
+    "url": "/my-portfolio-react/static/media/toDoList.694f5817.webp"
   },
   {
     "revision": "9e8bfce4de1e3508a98fb887c593c11b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio-react/precache-manifest.fef99f54f60b01746f36cc3a45506c82.js"
+  "/my-portfolio-react/precache-manifest.bd29fd73132efba5707e71f3a386c015.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio-react/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
